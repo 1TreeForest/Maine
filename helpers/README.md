0a09cf244de96051564dd17758f8dd0cf23c9084
1TreeForest <1204572840@qq.com>
2023-03-31 14:19:24 +0800

    finished a simple script to gather coverage info (lineno for now) using static instrumentation

---
+++ helpers/README.md
@@ -0,0 +1,6 @@
+# Helpers
+Use these helpers for gatering coverage and so on.
+
+## Coverage.php
+
+- add `require_once(coverage.php)` to enable xdebug_coverage functions in a file.
---
+++ helpers/coverage.php
@@ -0,0 +1,28 @@
+<?php
+
+// 启用 Xdebug 的覆盖率收集功能
+xdebug_start_code_coverage();
+
+// 在脚本执行结束后获取覆盖率数据
+function getCoverage() {
+    $coverage = xdebug_get_code_coverage();
+    chdir('/var/www/html/tmp/coverage');
+
+    // 生成保存文件的文件名
+    $filename = basename($_SERVER['SCRIPT_FILENAME']) . '.' . microtime(true) . '.json';
+
+    // 将覆盖率数据保存为 JSON 格式并写入文件
+    file_put_contents($filename, json_encode($coverage, JSON_PRETTY_PRINT));
+
+    echo 'Coverage data saved to /var/www/html/tmp/coverage/' . $filename . PHP_EOL;
+
+    var_dump($coverage);
+
+    $current_dir = getcwd();
+    echo $current_dir;
+}
+
+register_shutdown_function('getCoverage');
+$current_dir = getcwd();
+echo $current_dir;
+?>
